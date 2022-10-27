e65386bd817b805e1ba28e532e0b2d676d883db2
sgrcn17 <trzcinski@student.agh.edu.pl>
2022-10-27 18:17:45 +0200

    colliderObject class

--- .game/.objects/colliderObject.py
+++
@@ -1,4 +0,0 @@
-class colliderObject:
-
-    def __init__(self):
-        pass
---
+++ .game/_objects/colliderObject.py
@@ -0,0 +1,5 @@
+import gameObject
+class colliderObject(gameObject):
+
+    def __init__(self, name, posX, posY):
+        super().__init__(name, posX, posY)
