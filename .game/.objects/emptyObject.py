728dddd37a40d3d808e0947288b2e2a91d4c992a
sgrcn17 <trzcinski@student.agh.edu.pl>
2022-10-27 18:17:52 +0200

    emptyObject class

--- .game/.objects/emptyObject.py
+++
@@ -1,6 +0,0 @@
-import gameObject
-
-class emptyObject(gameObject):
-
-    def __init__(self, posX, posY):
-        super().__init__(posX, posY)
---
+++ .game/_objects/emptyObject.py
@@ -0,0 +1,5 @@
+import gameObject
+class emptyObject(gameObject):
+
+    def __init__(self, name, posX, posY):
+        super().__init__(name, posX, posY)
