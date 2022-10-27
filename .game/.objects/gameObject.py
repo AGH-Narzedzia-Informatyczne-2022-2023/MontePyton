8cd21bad84906125cffce96f37c713a12eff14bf
sgrcn17 <trzcinski@student.agh.edu.pl>
2022-10-27 18:17:59 +0200

    main gameObject class

--- .game/.objects/gameObject.py
+++
@@ -1,7 +0,0 @@
-class gameObject:
-    _posX = 0
-    _posY = 0
-
-    def __init__(self, posX, posY):
-        _posX = posX
-        _posY = posY
---
+++ .game/_objects/gameObject.py
@@ -0,0 +1,8 @@
+class gameObject:
+    posX = 0
+    posY = 0
+    name = "new object"
+    def __init__(self, _name, _posX, _posY):
+        name = _name
+        posX = _posX
+        posY = _posY
