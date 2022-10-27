21a4fdafb9aaaf96d679694a00bc84c0b85699af
sgrcn17 <trzcinski@student.agh.edu.pl>
2022-10-27 17:03:37 +0200

    main loop

--- .game/main.py
+++ .game/main.py
@@ -1,3 +1,20 @@
 import pygame
 
-print(pygame.__version__)
+background_colour = (234, 212, 252)
+
+screen = pygame.display.set_mode((300, 300))
+
+pygame.display.set_caption('Platformer Game')
+
+screen.fill(background_colour)
+
+pygame.display.flip()
+
+running = True
+
+while running:
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            running = False
