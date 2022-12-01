743ea3b43420668ca88f80e93f980a847b13e491
DominikMrozek <116183022+DominikMrozek@users.noreply.github.com>
2022-12-01 19:04:27 +0100

    Create mainn.py

---
+++ mainn.py
@@ -0,0 +1,25 @@
+import pygame
+from _objects.gameObject import gameObject
+#adding functionality to our project
+
+background_colour = (234, 212, 252)
+
+screen = pygame.display.set_mode((300, 300))
+
+pygame.display.set_caption('Platformer Game')
+
+screen.fill(background_colour)
+
+pygame.display.flip()
+
+running = True
+
+obiekt = gameObject(22, 34)
+print(obiekt.posX)
+
+while running:
+
+    for event in pygame.event.get():
+
+        if event.type == pygame.QUIT:
+            running = False
