3e5ccba195077eb9916c35b5d92254c8a964426c
DominikMrozek <116183022+DominikMrozek@users.noreply.github.com>
2022-12-01 19:46:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame
 from _objects.gameObject import gameObject
 #adding functionality to our project
 
-background_colour = (234, 212, 252)
+background_colour = (234, 212, 4202137)
 
 screen = pygame.display.set_mode((300, 300))
 
